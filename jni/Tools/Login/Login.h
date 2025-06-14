8e62a0a7575058e4dd7829b9f2ea3d5cbd01f883
GemblungDavin <gemblungdavin@gmail.com>
2025-06-15 00:04:06 +0700

    Update Login.h

--- jni/Tools/Login/Login.h
+++ jni/Tools/Login/Login.h
@@ -1,14 +1,52 @@
 #pragma once
-#include <string>
+#include "curl/curl.h"
+#include "Tools.h"
+#include "json.hpp"
 
-inline std::string slotZ = "0";
-inline std::string inVip = "0"; 
-inline std::string autoskillsZ = ""; 
-inline std::string expired = "N/A"; 
-inline std::string battleData = "true"; 
-inline std::string clientManager = "XCode Local";
+using json = nlohmann::ordered_json;
 
-inline std::string g_Token = "", g_Auth = "";
+std::string title, version;
+std::string UUID;
+
+static std::string slotZ = "";
+static std::string inVip = "";
+static std::string autoskillsZ = "";
+static std::string expired = "";
+static std::string EXP = "1212";
+static std::string name ="";
+static std::string device = "";
+static std::string status = "";
+static std::string floating ="";
+static std::string battleData = "true";
+static std::string clientManager = "";
+
+std::string g_Token, g_Auth;
+bool bValid = false;
+
+bool bInitDone;
+
+namespace SignIN
+{
+    static bool user;
+}
+
+struct MemoryStruct {
+    char *memory;
+    size_t size;
+};
+
+static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    struct MemoryStruct *mem = (struct MemoryStruct *) userp;
+    mem->memory = (char *) realloc(mem->memory, mem->size + realsize + 1);
+    if (mem->memory == NULL) {
+        return 0;
+    }
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+    return realsize;
+}
 
 inline std::string Login(void* jvm, const char* user_key, bool* success) {
     battleData = "true";
